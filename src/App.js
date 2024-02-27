0a280247fdfc2b1d788d4561dc7ee2bee117134a
Rishabh Gaur <rsgaur884@gmail.com>
2024-02-27 12:00:34 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+function App() {
+  return (
+    <>
+      <Navbar 
+        // title="textUtils"
+        aboutText="this is a simple example of a text manipulation service"
+      />
+    </>
+  );
+}
+
+export default App;
